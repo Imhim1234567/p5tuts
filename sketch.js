237fcdb59829483034f4b177f0f4ed63e63fab05
Stephen <sequan@cannonschool.org>
2023-11-30 10:42:41 -0500

    new

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220, 100, 100);
+
+  var x = map(noise(xoff), 0,1,0, width);
+  var y = map(noise(xoff), 0,1,0, width);
+
+  xoff += 0.02;
+
+
+  ellipse(x,y,24,24)
+
+}
