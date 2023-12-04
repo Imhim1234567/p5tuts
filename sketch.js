c7ae7fcd7d95418dbf78ddbfe3d6f39458fe1347
Stephen <sequan@cannonschool.org>
2023-12-04 10:43:13 -0500

    xoff start

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 //var xoff1 = 0;
 //var xoff2 = 10000;
+var inc = 0.01;
+ var start = 0;
 function setup() {
   createCanvas(400, 400);
 }
@@ -9,17 +11,20 @@ function draw() {
   stroke(255);
   noFill();
   beginShape();
-  var xoff = 0;
+  var xoff = start;
   for (var x = 0; x < width; x++){
     //var y = random(height);
     var  y = noise(xoff)*height;
     vertex(x,y);
     stroke(255);
 
-    xoff += 0.02;
+    xoff += inc;
+
   }
+
+  start += inc;
   endShape(); 
-  noLoop();
+  //noLoop();
 
 //  var x = map(noise(xoff1), 0,1,0, width);
   //var y = map(noise(xoff2), 0,1,0, height);
