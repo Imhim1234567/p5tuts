7d05d69e6275c6f51c7d8da5d11cfc16bcc58268
Stephen <sequan@cannonschool.org>
2023-12-07 10:47:51 -0500

    blue

--- sketch.js
+++ sketch.js
@@ -1,43 +1,29 @@
-//var xoff1 = 0;
-//var xoff2 = 10000;
-var inc = 0.01;
- var start = 0;
+var n = 0;
+var c = 2;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(2000, 1000);
+  angleMode(DEGREES);
+  colorMode(HSB);
+  background(0);
 }
 
 function draw() {
-  background(100, 156, 100);
-  stroke(255);
-  noFill();
-  beginShape();
-  var xoff = start;
-  for (var x = 0; x < width; x++){
-    var n = map(noise(xoff), 0, 1, 0, height);
-    var s = map(sin(xoff), -1, 1, -50, 50);
-    var y = s + n;
+  var  a = n * 137.5
+  var r = c * sqrt(n);
 
-    //var y = random(height);
-    //var  y = sin(xoff)*height;
-    vertex(x,y);
-    stroke(255);
+  var x = r * cos(a) + width/3;
+  var y = r * sin(a) + height/3;
+  fill(255 ,134, r );
+  noStroke();
 
-    xoff += inc;
+  ellipse(x,y,4,4)
 
+
+  n+=5
   }
 
-  start += inc;
-  endShape(); 
-  //noLoop();
-
-//  var x = map(noise(xoff1), 0,1,0, width);
-  //var y = map(noise(xoff2), 0,1,0, height);
-
-  //xoff1 += 0.02;
-  //xoff2 += 0.02;
 
 
 
-  //ellipse(x,y,24,24)
 
-}
+
